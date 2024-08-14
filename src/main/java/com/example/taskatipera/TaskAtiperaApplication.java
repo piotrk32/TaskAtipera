b2fa854891699b1f4da0942aeae5fb9c22f1e15f
piotrk32 <piotrkepistyuwm@gmail.com>
2024-08-14 19:13:32 +0200

    initial commit

---
+++ src/main/java/com/example/taskatipera/TaskAtiperaApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskatipera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskAtiperaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskAtiperaApplication.class, args);
+    }
+
+}
