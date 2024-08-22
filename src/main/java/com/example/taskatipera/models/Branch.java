48dafda62042fcdcf58ecc2dd1c2f6d511a48821
piotrk32 <piotrkepistyuwm@gmail.com>
2024-08-22 16:21:00 +0200

    models created

---
+++ src/main/java/com/example/taskatipera/models/Branch.java
@@ -0,0 +1,14 @@
+package com.example.taskatipera.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Branch {
+
+    private String name;
+    private Commit commit;
+
+
+}
---
+++ src/main/java/com/example/taskatipera/models/Commit.java
@@ -0,0 +1,10 @@
+package com.example.taskatipera.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Commit {
+    private String sha;
+}
